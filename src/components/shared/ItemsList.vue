f6aac0cce2a092a4e0632148d0dda58528a692a9
Shai Ament <shai@aphili.link>
2022-08-08 22:47:42 +0300

    item list styling and enter slicing

--- src/components/shared/ItemsList.vue
+++ src/components/shared/ItemsList.vue
@@ -1,10 +1,11 @@
 <template>
     <ul class="w-10/12 flex flex-col">
-        <li v-for="(item, idx) in items" class="flex row group" :key="idx">
+        <li v-for="(item, idx) in items" class="flex row group focus-within:border-y focus-within:border-y-zinc-300"
+            :key="idx">
             <div class="flex row">
-                <div class="hover:opacity-90 hover:bg-slate-200 rounded-full cursor-pointer">
+                <div class="active:opacity-90 active:bg-slate-200 rounded-full cursor-pointer">
                     <svg xmlns=" http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#000"
-                        class="invisible group-hover:visible" @click="() => { deleteItem(idx) }">
+                        class="invisible group-focus-within:visible" @clicked="() => { deleteItem(idx) }">
                         <path
                             d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
                     </svg>
@@ -14,14 +15,13 @@
                 </div>
             </div>
             <div>
-                <p v-if="idx === items.length - 1" id="newItemAdded" @keyup.enter.prevent="enterPressed"
-                    :data-index="idx" @input.prevent="updateItem" :class="{ 'line-through': item.done }" contenteditable
-                    class="outline-0">{{
+                <p v-if="idx === items.length - 1" id="newItemAdded" :data-index="idx" @input.prevent="updateItem"
+                    :class="{ 'line-through': item.done }" contenteditable class="outline-0">{{
                             item.title
                     }}
                 </p>
-                <p v-else :data-index="idx" @keyup.enter.prevent="enterPressed" :class="{ 'line-through': item.done }"
-                    @input="updateItem" contenteditable class="outline-0">
+                <p v-else :data-index="idx" :class="{ 'line-through': item.done }" @input.prevent="updateItem"
+                    contenteditable class="outline-0">
                     {{ item.title }}</p>
             </div>
         </li>
@@ -31,8 +31,8 @@
                 <path d="m38 26h-12v12h-4v-12h-12v-4h12v-12h4v12h12v4z" />
                 <path d="m0 0h48v48h-48z" fill="none" />
             </svg>
-            <div class="absolute inline  lw-full  pointer-events-none text-gray-500 left-[52px]">List Item</div>
-            <div contenteditable @keyup="newItem" class="outline-0 absolute inline w-16 left-[52px]" spellcheck="false">
+            <div class="absolute inline  lw-full  pointer-events-none text-gray-500 left-[52px]">Add new item</div>
+            <div contenteditable @input="newItem" class="outline-0 absolute inline w-28 left-[52px]" spellcheck="false">
             </div>
         </li>
     </ul>
@@ -45,29 +45,44 @@ const props = defineProps({
     items: { type: Array as PropType<IListItem[]>, required: true }
 })
 const emits = defineEmits(['updatedList']);
-const newItem = (e: KeyboardEvent) => {
-    if (e.key.length > 1) return;
-    emitUpdatedList(props.items.concat([{ title: e.key, done: false }]));
-    e.target.innerHTML = '';
-    focusNewItem = true;
+const newItem = (e: InputEvent) => {
+    if (e.data) {
+        emitUpdatedList(props.items.concat([{ title: e.data, done: false }]));
+        focusNewItem = true;
+    }
+    if (e.target) {
+        (e.target as HTMLDivElement).innerHTML = '';
+    }
+
+
 };
 const updateItemState = (idx: number) => {
     props.items[Number(idx)].done = !props.items[Number(idx)].done;
     emitUpdatedList(props.items);
 }
-const updateItem = (e: any) => {
+const updateItem = (e: InputEvent) => {
     const element = e.target as HTMLParagraphElement;
-    const idx = element.getAttribute('data-index');
+    const idx = Number(element.getAttribute('data-index'));
+    if (e.inputType === 'insertParagraph') {
+        const values = element.innerText.split('\n');
+        element.innerText = values[0];
+        let newItem = { title: values[1], done: false };
+        if (idx + 1 === props.items.length) {
+            props.items.push(newItem);
+        } else {
+            props.items.splice(idx, 1, newItem);
+        }
+        emitUpdatedList(props.items);
+    }
+
+
     if (idx) {
-        props.items[Number(idx)].title = element.innerText;
+        props.items[idx].title = element.innerText;
         emitUpdatedList(props.items);
     }
 
 }
-const enterPressed = (e: any) => {
-    emitUpdatedList(props.items.concat([{ title: '', done: false }]));
-    focusNewItem = true;
-}
+
 const deleteItem = (idx: number) => {
     props.items.splice(idx, 1);
     emitUpdatedList(props.items);
