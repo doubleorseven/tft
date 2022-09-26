b5c2c547b629da9a4db261701623f82dee4af314
Shai Ament <shai@aphili.link>
2022-09-26 23:29:41 +0300

    game modal and some logic

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,3 +1,4 @@
+import { setTransitionHooks } from "vue";
 import BaseClass from "./BaseClass";
 import type { ChooseTaskStarterModelData } from "./Task";
 
@@ -6,11 +7,24 @@ export default class Game extends BaseClass {
     public gameActive: boolean = true;
     public gameStarted: boolean = false;
     public estimatedEndDate?: number;
-    public tasksIds: string[];
+    private tasksIds: string[];
+    private cursor: number = 0;
     public data: ChooseTaskStarterModelData;
     constructor(tasksIds: string[], data: ChooseTaskStarterModelData) {
         super();
         this.tasksIds = tasksIds;
         this.data = data;
+    }
+    loadNextTask(): string {
+        if (this.cursor + 1 === this.tasksIds.length) {
+            this.cursor == 0;
+        }
+        return this.getCurrentTask();
+    }
+    getCurrentTask(): string {
+        return this.tasksIds[this.cursor];
+    }
+    getTaskLocation() {
+        return [this.cursor + 1, this.tasksIds.length] as const;
     }
 }
